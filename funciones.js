73e1ee7444aa7644c2f314062d7348eabde52677
JoADM <83179068+JoADM@users.noreply.github.com>
2022-09-07 22:26:17 -0300

    asd

--- funciones.js
+++ funciones.js
@@ -1,20 +1,38 @@
-function validar(){
-    let usuario ="simona"; 
-    let pass = "cacarisa";
-    if(document.getElementById('usr').value == usuario && document.getElementById('pass').value == pass){
+var datosUsuarios = {
+    usuario: '',
+    password: '',
+    nombre: '',
+    apellido: '',
+    edad: '',
+    profesion: ''
+}
+
+
+function validar() {
+    let usr = document.getElementById('usr').value;
+    let pass = document.getElementById('pass').value;
+
+    if (usr == datosUsuarios.usuario && pass == datosUsuarios.password) {
         alert("Bienvenido");
     } else {
         document.getElementById('error').innerHTML = "Usuario o contrase&ntilde;a incorrecta";
-        alert("Usuario o contrase\361a incorrecta");
-        
     }
 }
 
-function validarRegistro(){
-    if (document.getElementById('pass1').value == document.getElementById('pass2').value && document.getElementById('edad').value >= 1 && document.getElementById('captcha').value == "V4l1d4r R3g15tr0") {
-        alert("Registro exitoso!");
-        window.location.href = "./index.html"
+function validarRegistro() {
+    let EDAD_MINIMA = 1;
+
+    let usr = document.getElementById('user').value;
+    let pass1 = document.getElementById('pass').value;
+    let pass2 = document.getElementById('pass2').value;
+    let edad = document.getElementById('edad').value;
+    let lblcaptcha = document.getElementById('captcha').value;
+
+    let captcha = "V4l1d4r R3g15tr0";
+
+    if (pass1 == pass2 && edad >= EDAD_MINIMA && lblcaptcha == captcha) {
+        datosUsuarios.usuario = 
     } else {
-        alert("Revise los datos ingresados e intente nuevamente.")
+        alert("Revise los datos ingresados e intente nuevamente.");
     }
 }
