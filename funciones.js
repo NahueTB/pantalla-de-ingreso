e926c332e03e5ab3fabedaf0a2de2b0e393a3c38
JoADM <83179068+JoADM@users.noreply.github.com>
2022-09-14 22:00:30 -0300

    asd

--- funciones.js
+++ funciones.js
@@ -9,9 +9,12 @@ var datosUsuarios = {
 
 function validar() {
     let usr = document.getElementById('usr').value;
-    let pass = document.getElementById('pass').value;
+    let pass = document.getElementById('pass-login').value;
 
-    if (usr == datosUsuarios.usuario && pass == datosUsuarios.password) {
+    let usrComp = datosUsuarios.usuario;
+    let passComp = datosUsuarios.password;
+
+    if (usr == usrComp && pass == passComp) {
         alert("Bienvenido");
     } else {
         document.getElementById('error').innerHTML = "Usuario o contrase&ntilde;a incorrecta";
@@ -22,22 +25,24 @@ function validarRegistro() {
     let EDAD_MINIMA = 1;
 
     let usr = document.getElementById('user').value;
-    let pass1 = document.getElementById('pass').value;
-    let pass2 = document.getElementById('pass2').value;
+    let pass1 = document.getElementById('pass-register').value;
+    let pass2 = document.getElementById('pass2-register').value;
     let edad = document.getElementById('edad').value;
     let nombre = document.getElementById('nombre').value;
+    let apellido = document.getElementById('apellido').value;
     let mail = document.getElementById('mail').value;
     let lblcaptcha = document.getElementById('captcha').value;
 
     let captcha = "V4l1d4r R3g15tr0";
 
-    if (pass1 == pass2 && edad >= EDAD_MINIMA && lblcaptcha == captcha) {
+    if ((pass1 == pass2) && (edad >= EDAD_MINIMA) && (lblcaptcha == captcha)) {
         datosUsuarios.usuario = usr;
         datosUsuarios.password = pass1;
         datosUsuarios.nombre = nombre;
         datosUsuarios.apellido = apellido;
         datosUsuarios.mail = mail;
         datosUsuarios.edad = edad;
+        alert("biena hi");
     } else {
         alert("Revise los datos ingresados e intente nuevamente.");
     }
